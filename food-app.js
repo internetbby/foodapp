009713e264a0952c238a36825a2be606b14304aa
Linnea <linnea.alse@elev.ga.ntig.se>
2021-05-06 16:05:35 +0200

    added so you get more than one recipe

--- food-app.js
+++ food-app.js
@@ -1,16 +1,12 @@
 const button = document.querySelector("input#submit");
 const APIKey = "f547939e371f49cdbb08bd54ce24fe23";
 
-/*let slider = document.getElementById("prepTime");
-let outputForSlider = document.getElementById("outputForPrep");
+const deleteButton = document.querySelector("input#delete");
 
-outputForSlider.innerHTML = slider.value + " minutes";
-
-slider.oninput = function (){
-  outputForSlider.innerHTML=this.value + " minutes"; 
-}*/
+let buttonHitCounter = 0 ; //keeps track on how many times button has been hit
 
 button.addEventListener("click", buttonHit);
+deleteButton.addEventListener("click", deleteButtonHit);
 
 function buttonHit() {
   let diet = "";
@@ -25,47 +21,56 @@ function buttonHit() {
   let forming = typedIngredients.split(" ").join(",");
   console.log(forming);
 
-
-
   fetch(
-    /*`https://api.spoonacular.com/recipes/random?tags=${ingredients}&apiKey=${APIKey}`*/
-    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${APIKey}&diet=${diet}&ignorePantry=true&includeIngredients=${forming}&addRecipeInformation=true`
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${APIKey}&diet=${diet}&ignorePantry=true&includeIngredients=${forming}&addRecipeInformation=true&`
   )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
 
       let newDiv = document.createElement("div");
-
+      newDiv.classList.add("newDiv")
+      
       let title = document.createElement("h2");
+      title.classList.add("title")
 
-      title.innerText = data.results[0].title;
-      console.log(data.results[0].title);
+      if (data.results.offset == 0 ){
+        title.innerText = "no recipe found";
+      }
+
+      title.innerText = data.results[buttonHitCounter].title;
+      console.log(data.results[buttonHitCounter].title);
+
+     
 
       newDiv.append(title); // gives title
 
-      let ingredients= document.createElement("p")
-      for (n=0; n < data.results[0].analyzedInstructions[0].steps.length; n++){
-        for (i=0; i < data.results[0].analyzedInstructions[0].steps[n].ingredients.length; i++){
-          ingredients.textContent += " " +(data.results[0].analyzedInstructions[0].steps[n].ingredients[i].name);
+      let ingredients = document.createElement("p");
+      for (
+        n = 0; n < data.results[buttonHitCounter].analyzedInstructions[0].steps.length; n++) {
+        for (i = 0;i <data.results[buttonHitCounter].analyzedInstructions[0].steps[n].ingredients.length; i++) {
+          ingredients.textContent +=" " +data.results[buttonHitCounter].analyzedInstructions[0].steps[n].ingredients[i].name;
           ingredients.classList.add("ingredients");
-
         }
-     }
+      }
 
       let instructions = document.createElement("p");
 
-      for (i=0; i < data.results[0].analyzedInstructions[0].steps.length; i++){
-        instructions.textContent += (data.results[0].analyzedInstructions[0].steps[i].step);
-
+      for ( i = 0; i < data.results[buttonHitCounter].analyzedInstructions[0].steps.length; i++) {
+        instructions.textContent += data.results[buttonHitCounter].analyzedInstructions[0].steps[i].step;
       }
 
       newDiv.append(ingredients);
       newDiv.append(instructions);
 
-
-
-
       document.querySelector("body").append(newDiv);
+
+      buttonHitCounter++
     });
+}
+
+function deleteButtonHit(){
+  let deletedDiv = document.getElementsByClassName("newDiv");
+  document.getElementsByTagName.removeChild(newDiv);
+
 }
